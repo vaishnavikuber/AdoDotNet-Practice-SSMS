5c9cae988989e59327aba9c6af7ae3884c35c586
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-07 10:06:07 +0530

    [vaishnavi]added: customermodel class

---
+++ EmployeePayRollAdo.Net/CustomerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayRollAdo.Net
+{
+    internal class CustomerModel
+    {
+
+        public int CustomerId { get; set; }
+        public string CustomerName {  get; set; }
+        public string Email {  get; set; }
+        public string PhoneNumber {  get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender {  get; set; }
+
+    }
+}
