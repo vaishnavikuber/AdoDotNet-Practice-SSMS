d1ddd61c7b8ab28cf0100d6f387ef7debe86d8e5
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-07 10:08:15 +0530

    [vaishnavi]added: modyfied classes

--- EmployeePayRollAdo.Net/obj/Debug/netcoreapp3.1/EmployeePayRollAdo.Net.AssemblyInfo.cs
+++ EmployeePayRollAdo.Net/obj/Debug/netcoreapp3.1/EmployeePayRollAdo.Net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeePayRollAdo.Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1a67c86d6d9884034899952c1239a2af461f46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525fe664959874810a7dadc0cce42c56535334e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeePayRollAdo.Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeePayRollAdo.Net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
